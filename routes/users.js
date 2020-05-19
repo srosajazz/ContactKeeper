50e0dbf2988fcb6701a28e6053d86eb1d9c6841e
Sergio Rosa <scarlosjazz@gmail.com>
2020-05-19 12:29:32 -0400

    chore:  Create & Respond With JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const { check, validationResult } = require('express-validator');
 
@@ -44,7 +46,26 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.send('User saved');
+
+      //jwt - payload
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //Generete the token & expriration
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('ServerError');
